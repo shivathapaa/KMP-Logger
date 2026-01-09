422047787ee222df98d45e75392bc2625eb3b585
shivathapa <shivathapa0600@gmail.com>
2026-01-09 12:02:33 +0545

    Add Kotlin Multiplatform sample application.

---
+++ sample/composeApp/build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.composeMultiplatform)
+    alias(libs.plugins.android.application)
+}
+
+kotlin {
+    androidTarget()
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach {
+        it.binaries.framework {
+            baseName = "ComposeApp"
+            isStatic = true
+        }
+    }
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(compose.runtime)
+            implementation(compose.ui)
+            implementation(compose.foundation)
+            implementation(libs.material3)
+            implementation(project(":logger"))
+        }
+
+        androidMain.dependencies {
+            implementation(libs.androidx.activityCompose)
+        }
+
+    }
+}
+
+android {
+    namespace = "sample.app"
+    compileSdk = 36
+
+    defaultConfig {
+        minSdk = 24
+        targetSdk = 36
+
+        applicationId = "sample.app.androidApp"
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+}
+
