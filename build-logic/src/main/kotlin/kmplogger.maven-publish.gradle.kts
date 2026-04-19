62f80340b84bedfb67ae1db3a1dcd0e9163fa3c7
shivathapa <shivathapa0600@gmail.com>
2026-04-19 08:14:26 +0545

    build: introduce convention plugins in build-logic and migrate modules to shared multiplatform and publishing configuration

---
+++ build-logic/src/main/kotlin/kmplogger.maven-publish.gradle.kts
@@ -0,0 +1,49 @@
+import com.vanniktech.maven.publish.KotlinMultiplatform
+import com.vanniktech.maven.publish.SourcesJar
+
+plugins {
+    id("com.vanniktech.maven.publish")
+}
+
+mavenPublishing {
+    coordinates(
+        groupId = "io.github.shivathapaa",
+        artifactId = project.name,
+        version = providers.gradleProperty("kmplogger.version").get()
+    )
+
+    configure(KotlinMultiplatform(sourcesJar = SourcesJar.Sources()))
+
+    publishToMavenCentral()
+
+    if (providers.gradleProperty("signingInMemoryKey").isPresent ||
+        providers.environmentVariable("ORG_GRADLE_PROJECT_signingInMemoryKey").isPresent
+    ) {
+        signAllPublications()
+    }
+
+    pom {
+        name.set(project.name)
+        inceptionYear.set("2026")
+        url.set("https://github.com/shivathapaa/KMP-Logger")
+        licenses {
+            license {
+                name.set("MIT")
+                url.set("https://opensource.org/licenses/MIT")
+            }
+        }
+        developers {
+            developer {
+                id.set("shivathapaa")
+                name.set("Shiva Thapa")
+                email.set("query.shivathapaa.dev@gmail.com")
+                url.set("https://github.com/shivathapaa/")
+            }
+        }
+        scm {
+            url.set("https://github.com/shivathapaa/KMP-Logger")
+            connection.set("scm:git:git://github.com/shivathapaa/KMP-Logger.git")
+            developerConnection.set("scm:git:ssh://github.com/shivathapaa/KMP-Logger.git")
+        }
+    }
+}
